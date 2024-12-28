3023c43150d9a5b800b7ef2c764d8170170a0b24
J-LopezE <jorgeisrael.lopez@gmail.com>
2024-12-27 21:11:57 -0600

    login and files all

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ import injectContext from "../src/store/appContext.jsx";
 
 import Home from "./pages/home.jsx";
 import Navbar from "./components/navbar.jsx";
+import { Dashboard } from "./pages/dashboard.jsx";
+import { Tickets } from "./pages/tickets.jsx";
+import { Branches } from "./pages/branches.jsx";
+import { Client } from "./pages/client.jsx";
+import { Providers } from "./pages/providers.jsx";
+import { Ingeniers } from "./pages/ingeniers.jsx";
+import { Factures } from "./pages/factures.jsx";
 
 function App({ Component, pageProps }) {
   return (
@@ -15,6 +22,13 @@ function App({ Component, pageProps }) {
           <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/tickets" element={<Tickets />} />
+            <Route path="/branches" element={<Branches />} />
+            <Route path="/client" element={<Client />} />
+            <Route path="/providers" element={<Providers />} />
+            <Route path="/ingeniers" element={<Ingeniers />} />
+            <Route path="/factures" element={<Factures />} />
             <Route element={<h1>Not found!</h1>} />
           </Routes>
         </BrowserRouter>
